a597d02895376a8fc1f90f471de5ed29327e5b37
m0zu <m0zurillex@gmail.com>
2018-11-10 13:17:01 +0900

    The first commit message

---
+++ tweet_contribution.py
@@ -0,0 +1,25 @@
+import tweepy,requests,datetime
+from bs4 import BeautifulSoup as bs
+
+def get_contribution():
+    url = 'https://github.com/users/' + your_git_hub_id  + '/contributions'
+    req = requests.get(url)
+    soup = bs(req.content,'html.parser')
+    contribution = [i.get('data-count') for i in soup.find_all(class_="day")]
+    return contribution[-1]
+
+def tweet(text):
+    consumer_key = 'consumer_key'
+    access_key = 'access_key'
+    access_secret = 'access_secret'
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
+    api.update_status(text)
+
+if __name__ == '__main__':
+    day = datetime.date.today()
+    num = get_contribution()
+    text = '{}のcontribution数は{}でした #github'.format(day,num)
+    tweet(text)
+
