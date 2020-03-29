0b2fa921eda94b8013a5a8fdbf5b220c4ce8d3ef
0x0u <m0zurillex@gmail.com>
2020-03-29 20:06:36 +0900

    TwitterAPIのトークンを定数にした

--- main.py
+++ main.py
@@ -5,28 +5,32 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta, timezone
 
-def get_contribution(gh_id):
-    url = "https://github.com/users/" + gh_id + "/contributions"
+# githubのID
+GH_ID = sys.argv[1]
+
+# Tokens
+CONSUMER_KEY = os.environ.get("CONSUMER_KEY")
+CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET")
+ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
+
+def get_contribution():
+    url = "https://github.com/users/" + GH_ID + "/contributions"
     req = requests.get(url)
     soup = BeautifulSoup(req.content, "html.parser")
     return soup.find_all(class_="day")[-1]["data-count"]
 
 def tweet(text):
-    consumer_key = os.environ.get("consumer_key")
-    consumer_secret = os.environ.get("consumer_secret")
-    access_key = os.environ.get("access_key")
-    access_secret = os.environ.get("access_secret")
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_key, access_secret)
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     api = tweepy.API(auth)
     api.update_status(text)
 
 
 if __name__ == "__main__":
-    args = sys.argv
     jst = timezone(timedelta(hours=+9), "JST")
     today = datetime.now(jst).strftime("%Y-%m-%d")
-    contribution = get_contribution(args[1])
+    contribution = get_contribution()
     text = "{}のcontribution数は{}でした #github".format(today, contribution)
     tweet(text)
 
