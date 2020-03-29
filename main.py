791b3d58a2c59da7091df3cfdcf0da71529f52d7
0x0u <m0zurillex@gmail.com>
2020-03-29 19:45:19 +0900

    コードの刷新

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
-import tweepy,requests,datetime
-from bs4 import BeautifulSoup as bs
+import os
+import sys
+import tweepy
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta, timezone
 
-def get_contribution():
-    url = 'https://github.com/users/' + your_git_hub_id  + '/contributions'
+def get_contribution(gh_id):
+    url = "https://github.com/users/" + gh_id + "/contributions"
     req = requests.get(url)
-    soup = bs(req.content,'html.parser')
-    contribution = [i.get('data-count') for i in soup.find_all(class_="day")]
-    return contribution[-1]
+    soup = BeautifulSoup(req.content, "html.parser")
+    return soup.find_all(class_="day")[-1]["data-count"]
 
 def tweet(text):
-    consumer_key = 'consumer_key'
-    consumer_secret = 'consumer_secret'
-    access_key = 'access_key'
-    access_secret = 'access_secret'
+    consumer_key = os.environ.get("consumer_key")
+    consumer_secret = os.environ.get("consumer_secret")
+    access_key = os.environ.get("access_key")
+    access_secret = os.environ.get("access_secret")
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_key, access_secret)
     api = tweepy.API(auth)
     api.update_status(text)
 
-if __name__ == '__main__':
-    day = datetime.date.today()
-    num = get_contribution()
-    text = '{}のcontribution数は{}でした #github'.format(day,num)
+
+if __name__ == "__main__":
+    args = sys.argv
+    jst = timezone(timedelta(hours=+9), "JST")
+    today = datetime.now(jst).strftime("%Y-%m-%d")
+    contribution = get_contribution(args[1])
+    text = "{}のcontribution数は{}でした #github".format(today, contribution)
     tweet(text)
 
